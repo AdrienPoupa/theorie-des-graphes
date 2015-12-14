63d3e60f7d809e01836333a762e3367696fab354
Timote <timo.b35+gitlab@gmail.com>
2015-12-14 12:37:47 +0100

    transitive

--- buob-barbot-poupa.cpp
+++ buob-barbot-poupa.cpp
@@ -12,13 +12,16 @@ typedef struct {
     int ** MVal; // Si MAdj[x][y] = TRUE alors MVal[x][y] = valeur de l’arc (x,y)
 } t_graphe;
 
-void generateFromFile(t_graphe * target){
-    // Lecture du graphe sur fichier
-    ifstream fg (FICHIER_GRAPHE);
-    
-    // Lecture du nombre de sommets et allocation dynamique des SdD
-    fg >> target->nbSommets;
-    
+void generateMatriceVide(t_graphe * target, int nbSommets);
+void generateFromFile(t_graphe * target);
+void afficheCompletGraphe(t_graphe * target);
+void afficheMatriceAdjacente(t_graphe * target);
+void afficheMatriceIncidence(t_graphe * target);
+void transitive(t_graphe * original, t_graphe * target);
+
+
+void generateMatriceVide(t_graphe * target, int nbSommets){
+    target->nbSommets = nbSommets;
     target->MAdj = new bool * [target->nbSommets];
     for (int s = 0; s < target->nbSommets; s++) {
         target->MAdj[s] = new bool [target->nbSommets];
@@ -35,6 +38,18 @@ void generateFromFile(t_graphe * target){
             target->MVal[s][extTerm] = 0;
         }
     }
+}
+
+void generateFromFile(t_graphe * target){
+    // Lecture du graphe sur fichier
+    ifstream fg (FICHIER_GRAPHE);
+    
+    int nbSommets;
+    
+    // Lecture du nombre de sommets et allocation dynamique des SdD
+    fg >> nbSommets;
+    
+    generateMatriceVide(target, nbSommets);
     
     // Lecture des arcs
     int extInit, extTerm, valeur;
@@ -50,7 +65,6 @@ void generateFromFile(t_graphe * target){
 }
 
 void afficheMatriceAdjacente(t_graphe * target){
-    // Impression graphe
     for (int x = 0; x < target->nbSommets; x++) {
         cout << x << "\t";
         for (int y = 0; y < target->nbSommets; y ++) {
@@ -83,13 +97,60 @@ void afficheCompletGraphe(t_graphe * target){
     afficheMatriceIncidence(target);
 }
 
+void transitive(t_graphe * original, t_graphe * target){
+    t_graphe * m = new t_graphe;
+    
+    generateMatriceVide(m, original->nbSommets);
+    generateMatriceVide(target, original->nbSommets);
+    
+    for(int n = 2; n <= original->nbSommets - 1; n++){
+        for (int i = 0; i < m->nbSommets; i++) {
+            for(int j = 0; j < m->nbSommets; j++){
+                for(int k = 0; k < m->nbSommets; k++){
+                    if(n == 2){
+                        m->MAdj[i][j] += (bool)((int)original->MAdj[i][k] * (int)original->MAdj[k][j]);
+                    }
+                    else{
+                        m->MAdj[i][j] += (bool)((int)original->MAdj[i][k] * (int)m->MAdj[k][j]);
+                    }
+                }
+            }
+        }
+        
+        cout << "m^" << n <<" : " << endl;
+        afficheMatriceAdjacente(m);
+        
+        for (int i = 0; i < m->nbSommets; i++){
+            for(int j = 0; j < m->nbSommets; j ++){
+                if(n == 2){
+                    target->MAdj[i][j] = m->MAdj[i][j] || original->MAdj[i][j];
+                }
+                else{
+                    target->MAdj[i][j] = m->MAdj[i][j] || target->MAdj[i][j];
+                }
+            }
+        }
+        
+        cout << "transitive pour n=" << n << " : " << endl;
+        afficheMatriceAdjacente(target);
+    }
+}
+
 int main () {
     // Déclaration graphe
     t_graphe * G = new t_graphe;
 
     generateFromFile(G);
     
-    afficheCompletGraphe(G);
+    cout << "original: " << endl;
+    
+    afficheMatriceAdjacente(G);
+    
+    //afficheCompletGraphe(G);
 
+    t_graphe * t = new t_graphe;
+    
+    transitive(G, t);
+    
     return 1;
 }
