d37418d7853861599f65366ef943710339191f71
Timote <timo.b35+gitlab@gmail.com>
2015-12-14 10:44:22 +0100

    creation fonction generation et affichage

--- buob-barbot-poupa.cpp
+++ buob-barbot-poupa.cpp
@@ -12,50 +12,49 @@ typedef struct {
     int ** MVal; // Si MAdj[x][y] = TRUE alors MVal[x][y] = valeur de l’arc (x,y)
 } t_graphe;
 
-int main () {
-    // Déclaration graphe
-    t_graphe * G = new t_graphe;
-
+void generateFromFile(t_graphe * target){
     // Lecture du graphe sur fichier
     ifstream fg (FICHIER_GRAPHE);
-
+    
     // Lecture du nombre de sommets et allocation dynamique des SdD
-    fg >> G->nbSommets;
-
-    G->MAdj = new bool * [G->nbSommets];
-    for (int s = 0; s < G->nbSommets; s++) {
-        G->MAdj[s] = new bool [G->nbSommets];
-        for (int extTerm = 0; extTerm < G->nbSommets; extTerm++) {
-            G->MAdj[s][extTerm] = false;
+    fg >> target->nbSommets;
+    
+    target->MAdj = new bool * [target->nbSommets];
+    for (int s = 0; s < target->nbSommets; s++) {
+        target->MAdj[s] = new bool [target->nbSommets];
+        for (int extTerm = 0; extTerm < target->nbSommets; extTerm++) {
+            target->MAdj[s][extTerm] = false;
         }
     }
-
-    G->MVal = new int * [G->nbSommets];
-
-    for (int s = 0; s < G->nbSommets; s++) {
-        G->MVal[s] = new int [G->nbSommets];
-        for (int extTerm = 0; extTerm < G->nbSommets; extTerm++) {
-            G->MVal[s][extTerm] = 0;
+    
+    target->MVal = new int * [target->nbSommets];
+    
+    for (int s = 0; s < target->nbSommets; s++) {
+        target->MVal[s] = new int [target->nbSommets];
+        for (int extTerm = 0; extTerm < target->nbSommets; extTerm++) {
+            target->MVal[s][extTerm] = 0;
         }
     }
-
+    
     // Lecture des arcs
     int extInit, extTerm, valeur;
-
+    
     fg >> extInit;
     while (extInit != -1) {
         fg >> extTerm;
         fg >> valeur;
-        G->MAdj[extInit][extTerm] = true;
-        G->MVal[extInit][extTerm] = valeur;
+        target->MAdj[extInit][extTerm] = true;
+        target->MVal[extInit][extTerm] = valeur;
         fg >> extInit;
     }
+}
 
+void afficheMatriceAdjacente(t_graphe * target){
     // Impression graphe
-    for (int x = 0; x < G->nbSommets; x++) {
+    for (int x = 0; x < target->nbSommets; x++) {
         cout << x << "\t";
-        for (int y = 0; y < G->nbSommets; y ++) {
-            if (G->MAdj[x][y] == true) {
+        for (int y = 0; y < target->nbSommets; y ++) {
+            if (target->MAdj[x][y] == true) {
                 cout << "X\t";
             } else {
                 cout << "\t";
@@ -63,19 +62,34 @@ int main () {
         }
         cout << endl;
     }
+}
 
-    for (int x = 0; x < G->nbSommets; x++) {
+void afficheMatriceIncidence(t_graphe * target){
+    for (int x = 0; x < target->nbSommets; x++) {
         cout << x << "\t";
-        for (int y = 0; y < G->nbSommets; y ++) {
-            if (G->MAdj[x][y] == true) {
-                cout << G->MVal[x][y] << "\t";
+        for (int y = 0; y < target->nbSommets; y ++) {
+            if (target->MAdj[x][y] == true) {
+                cout << target->MVal[x][y] << "\t";
             } else {
                 cout << "\t";
             }
         }
         cout << endl;
     }
+}
 
-    system("PAUSE");
+void afficheCompletGraphe(t_graphe * target){
+    afficheMatriceAdjacente(target);
+    afficheMatriceIncidence(target);
+}
+
+int main () {
+    // Déclaration graphe
+    t_graphe * G = new t_graphe;
+
+    generateFromFile(G);
+    
+    afficheCompletGraphe(G);
+
     return 1;
 }
